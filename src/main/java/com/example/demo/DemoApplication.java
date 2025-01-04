0e67f286986a1dd2851a9abeda0d5f52f6d7e8f1
Rajasekhar Reddy <rreddy@arraynetworks.net>
2025-01-04 20:50:51 +0530

    initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,6 +18,6 @@ class HelloController{
 	@GetMapping("/")
 	public String hello()
 	{
-		return "Hello, This is Spring  Boot Project App";
+		return "Hello, This is Spring  Boot Project for cloud";
 	}
 }
