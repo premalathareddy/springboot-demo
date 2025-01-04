2cd181c7e92fbce71ffd152e48364143d1cc9f25
Rajasekhar Reddy <rreddy@arraynetworks.net>
2025-01-04 13:57:25 +0530

    intial commit for jenkins pipeline one

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,6 +18,6 @@ class HelloController{
 	@GetMapping("/")
 	public String hello()
 	{
-		return "Hello, This is Spring  Boot Application For cloud";
+		return "Hello, This is Spring  Boot Application For cloud.";
 	}
 }
